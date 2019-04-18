367d2866777894ec790ff124e6850f18a582de39
LVTTL <contact@lvttl.xyz>
2019-04-18 18:18:48 +0200

    changed sampleCode to include some parameters

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 #include "gcodeParser.hpp"
 
-const std::string sampleCode = "N1 G00#N2 G10#N3 G12#N4 G20#";
+const std::string sampleCode = "N1 G00 X43#N2 G10#N3 G12 Z10#N4 G20#";
 
 GCodeParser gp;
 
